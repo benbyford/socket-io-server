cbfaa10d95e62c05ba1c5a3c039bdb10f8215d4c
Ben Mac mini <benjaminbyford@gmail.com>
2022-05-11 13:12:44 +0100

    git ignore added

--- app.js
+++ app.js
@@ -1,86 +1,125 @@
 console.log("Starting up");
 
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-
-app.use(express.static(__dirname + '/node_modules'));
-app.get('/', function(req, res,next) {
-    res.sendFile(__dirname + '/index.html');
-});
-
-// imports
-// var json = require('express/lib/response')
-// var app = express();
-// var httpServer = require('http').createServer();
-// var io = require('socket.io')(http);
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const cors = require("cors");
+const { log } = require('console');
 
 console.log("Finshed imports");
 
-// local code
-// var receive = require('./receive');
+/*
+* Server setup
+*/
+
+// Set up our server so it will listen on the port
+port = 8080;
+server.listen(port, function (error) {
+    // Checking any error occur while listening on port
+    if (error) {
+        console.log('Something went wrong', error);
+    }else {
+        console.log('Server is listening on port ' + port);
+    }
+});
+
+/*
+* Express setup
+*/
+app.use(express.static(__dirname + '/node_modules'));
+app.use(express.static('public')); // use public folder for assets in html
+app.use(cors());
+
+// routes
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
 
 
-// var connection = null;
 
-// // stat managemen
-// var stat = {};
+/*
+* utilties 
+*/ 
+
+// log something
+const logAll = (arr, name = null) => {
+    console.log("***");
+    if(name) console.log(name);
+
+    if(arr.length){
+        arr.forEach( (item) => {
+            console.log(item);
+        });
+    }
+}
+// add user to room
+const addRoom = id => {
+    // check its a new room
+    if(socketStat.rooms.indexOf(id) == -1) socketStat.rooms.push(id);
+}
+const addUser = id => {
+    socketStat.peers.push(id);
+}
+const removeUser = id => {
+    socketStat.peers.pop(id);
+}
+// log all rooms
+const showAllRooms = () => {
+    logAll(socketStat.rooms, "Rooms")
+}
 
 
-// // general err handling
-// var throwErr = function(err, result){
-//     if (err) throw err;
-//     console.log(JSON.stringify(result, null, 2));
-// }
+/*
+* Socket stuff
+*/
+
+// stat management
+const socketStat = {};
+
+socketStat.rooms = [];
+socketStat.peers = [];
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+
+    socket.on("join",(roomId) =>{
+        socket.join(roomId);
+        socket.emit("joined", roomId);
+        socket.data.room = roomId;
+
+        addUser(socket.id);
+        addRoom(roomId);
+
+        showAll();
+        showAllRooms();
+        
+        socket.to(roomId).emit("enter", "peer entered")
+    });
+
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+
+    socket.on('messages', function(msg){
+        // add data
+        socket.to(socket.data.room).emit("updates", msg)
+    });    
+});
+
+// todo
+// track num users in rooms, remove rooms if empty
 
 
-// /*
-// * Express setup
-// */
 
-// // use public folder for assets in html
-// app.use(express.static('public'));
+async function showAll(){
+    const sockets = await io.fetchSockets();
+    console.log("***");
+    console.log("num of users: ",sockets.length);
 
-// // listen to requests
-// app.get('/', function(req, res){
-//     res.sendFile(__dirname + '/index.html');
-// });
-
-// // connection listen
-// http.listen(3000, function(){
-//     // log after server start
-//     console.log('SERVER - Listening on *:3000');
-// });
-
-// /*
-// * Websocket
-// */
-
-// // delete all items within table
-// // r.db('poker').table('games').delete().run();
-
-// io.on('connection', function(socket){
-//   console.log('a user connected');
-
-// //   socket.on('disconnect', function(){
-// //     console.log('user disconnected');
-// //   });
-
-// //   socket.on('message', function(msg){
-// //       // add data
-// //       r.db('poker').table('games').insert([
-// //           {
-// //               message: msg
-// //           }
-// //       ]).run(connection, function(err, result) { throwErr(err, result) });
-
-// //       // run game logic after message and
-// //       // report back to the user message logged
-
-// //       var returnMsg = pokerLogic();
-// //       io.emit('message',returnMsg);
-
-// //       console.log('message: ' + msg);
-// //   });
-// });
+    for (const socket of sockets) {
+        console.log("id:",socket.id);
+        console.log("rooms:",socket.rooms);
+        console.log("data:",socket.data);
+    }
+}
